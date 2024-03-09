e774c3942828cd0533112a796bd06fc1b5519df0
Justin <Justin McIntyre>
2024-03-09 13:02:43 -0400

    added default export path

--- src/lib/Exporter.svelte
+++ src/lib/Exporter.svelte
@@ -2,11 +2,39 @@
     import { writeText } from '@tauri-apps/api/clipboard';
     import { save } from '@tauri-apps/api/dialog';
     import { writeTextFile } from '@tauri-apps/api/fs';
+    import { SettingsManager } from 'tauri-settings';
+
+    type Schema = {
+        defaultExportPath: string
+    }
+
+    const settingsManager = new SettingsManager<Schema>(
+        { // defaults
+            defaultExportPath: "bingo"
+        },
+        { // options
+            fileName: 'user-settings'
+        }
+    )
+
+    let defaultExportPath = ''
+    settingsManager.initialize().then(async()=>{
+        await settingsManager.syncCache()
+        defaultExportPath = settingsManager.getCache('defaultExportPath')
+    })
+
 
     export let csvs: {ticker: string, csv: string}[] = []
+
 </script>
 
-<div style="display: flex; gap: 20px;">
+<div class="container" style="display: flex; gap: 20px;">
+    
+    <div>
+        <label>default export path</label>
+        <input bind:value={defaultExportPath} />
+    </div>
+
     {#each csvs as csv}
         <div class="container">
             <h3>{csv.ticker.toUpperCase()}</h3>
@@ -15,8 +43,10 @@
                 alert(csv.ticker + " csv copied to clipboard")
             }}>copy</button>
             <button on:click={async ()=>{
+                settingsManager.setCache("defaultExportPath", defaultExportPath)
+                await settingsManager.syncCache()
                 const filePath = await save({
-                    defaultPath: `~/Documents/${csv.ticker}-data.csv`,
+                    defaultPath: `${defaultExportPath}\\${csv.ticker}-data.csv`,
                     filters: [{
                         name: 'csv',
                         extensions: ['csv'],
